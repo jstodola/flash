144cd3fe5a9bf7e7f53a31dd5cbba88f6ef7916c
Jan Stodola <honza.stodola@gmail.com>
2013-01-06 16:17:02 +0100

    button class can handle log key press and repeating events

--- button.cpp
+++ button.cpp
@@ -2,13 +2,38 @@
 #include "button.h"
 
 button::button(int pin) {
-    pinMode(pin, INPUT);
-    this->last_state = digitalRead(pin);
-    this->pin = pin;
-    this->last_debounce_time = millis();
+    // default debounce time is 50 ms,
+    // wait 1000 ms before repeating, repeat 4 times per second
+    init(pin, debounce_delay_def, repeat_delay_def, repeat_rete_def);
 }
 
-int button::get_state() {
+button::button(int pin, int debounce_delay, int repeat_delay, int repeat_rate) {
+    init(pin, debounce_delay, repeat_delay, repeat_rate);
+}
+
+void button::init(int pin, int debounce_delay, int repeat_delay, int repeat_rate) {
+    
+    int button_state;
+    unsigned long now;
+    
+    pinMode(pin, INPUT);
+
+    button_state = digitalRead(pin);
+    now = millis();
+
+    this->last_state = button_state;
+    this->last_stable_state = button_state;
+    this->pin = pin;
+    this->last_debounce_time = now;
+    this->returned_state_time = now;
+    this->debounce_delay = debounce_delay;
+    this->repeat_delay = repeat_delay;
+    this->repeat_rate = repeat_rate;
+    this->repeating = 0;
+}
+
+// returns state of button after debouncing
+int button::state() {
 
     int button_state;
     int reading;
@@ -18,7 +43,7 @@ int button::get_state() {
         this->last_debounce_time = millis();
     }
 
-    if((int)(millis() - this->last_debounce_time) > debounce_delay) {
+    if((int)(millis() - this->last_debounce_time) > this->debounce_delay) {
         button_state = reading;
     }else {
         button_state = this->last_state;
@@ -28,3 +53,38 @@ int button::get_state() {
     return(button_state);
 }
 
+// returns one HIGH value for each key press, also send HIGH for repeating
+// events when key is pressed for a long time
+int button::read() {
+    
+    int current_state;
+    int return_state = LOW;
+    int event_delay;
+    unsigned long now;
+
+    now = millis();
+    current_state = state();
+
+    if(current_state != this->last_stable_state) {
+        this->repeating = 0;
+        this->returned_state_time = now;
+        return_state = current_state;
+    } else {
+        if(this->repeating) {
+            event_delay = 1000 / this->repeat_rate;
+        } else {
+            event_delay = this->repeat_delay;
+        }
+        
+        // it is time to return another button event
+        if(this->returned_state_time + event_delay < now) {
+            this->repeating = 1;
+            this->returned_state_time = now;
+            return_state = current_state;
+        }
+
+    }
+
+    this->last_stable_state = current_state;
+    return return_state;
+}
--- button.h
+++ button.h
@@ -1,16 +1,27 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
-const int debounce_delay = 50;   // 50 ms
+const int debounce_delay_def = 50;   // 50 ms
+const int repeat_delay_def = 1000;   // 1000 ms
+const int repeat_rate_def = 4;       // 4 repeats per second
 
 class button {
   public:
     button(int pin);
-    int get_state();
+    button(int pin, int debounce_delay, int repeat_delay, int repeat_rate);
+    int state();
+    int read();
   private:
     int pin;
     int last_state;
+    int last_stable_state;
     unsigned long last_debounce_time;
+    unsigned long returned_state_time;
+    int debounce_delay;
+    int repeating;
+    int repeat_delay;
+    int repeat_rate;
+    void init(int pin, int debounce_delay, int repeat_delay, int repeat_rate);
 };
 
 #endif
