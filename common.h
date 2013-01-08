a49791fe6cf61e98331151a641fdbf609e16eee7
Jan Stodola <honza.stodola@gmail.com>
2013-01-08 22:56:32 +0100

    commonly used constants

---
+++ common.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+const uint8_t IDLE  = 0;
+const uint8_t UP    = 1;
+const uint8_t DOWN  = 2;
+const uint8_t LEFT  = 3;
+const uint8_t RIGHT = 4;
+const uint8_t OK    = 5;
+const uint8_t RC1   = 6;
+const uint8_t RC2   = 7;
+
+#endif
